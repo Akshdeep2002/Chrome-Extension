c3c35817e3aee79896a87b32b4127c1cd37a8269
Akshdeep Yadav <akshdeep3602@gmailcom>
2024-03-25 20:03:14 +0530

    first

---
+++ script.js
@@ -0,0 +1,32 @@
+async function getMatchData() {
+  //fetching cricket information from cricketdata.org
+  return await fetch(
+    "https://api.cricapi.com/v1/currentMatches?apikey=bef4b6e4-0fbb-4e11-9410-6e16e2ca5ab1&offset=0"
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      if (data.status != "success") return;
+
+      const matchesList = data.data;
+
+      if (!matchesList) return [];
+
+      //add your api key from cricketdata.org
+      const relevantData = matchesList
+        .filter(
+          (match) => match.series_id == "76ae85e2-88e5-4e99-83e4-5f352108aebc"
+        )
+        .map((match) => `${match.name}, ${match.status}`);
+
+      console.log({ relevantData });
+
+      document.getElementById("matches").innerHTML = relevantData
+        .map((match) => `<li>${match} </li>`)
+        .join("");
+
+      return relevantData;
+    })
+    .catch((e) => console.log(e));
+}
+
+getMatchData();
